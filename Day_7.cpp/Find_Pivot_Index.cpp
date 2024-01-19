9ba8493c3dc633451bdb6895f0119988932a63db
Vansh Vadhwa <vvad0647@gmail.com>
2024-01-19 10:24:29 +0530

    Add files via upload

---
+++ Day_7.cpp/Find_Pivot_Index.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        int leftSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i; 
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1; 
+    }
+};
