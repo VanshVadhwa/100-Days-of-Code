09bf1f463b2d3fe38e5f07415320b09bec965127
Vansh Vadhwa <vvad0647@gmail.com>
2024-01-25 22:56:59 +0530

    Add files via upload

---
+++ Day_13.cpp/Reverse_the_Array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void reverseArray (vector<int> &arr , int m) {  
+    int start=m+1;
+    int end=arr.size()-1;
+    while(start<=end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }    	
+}
