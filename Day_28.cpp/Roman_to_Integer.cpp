35b42df37c669e830a4f2797a44eced59a9a2d51
Vansh Vadhwa <vvad0647@gmail.com>
2024-02-09 21:26:51 +0530

    Add files via upload

---
+++ Day_28.cpp/Roman_to_Integer.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> romanMap = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int result = 0;
+        int preValue = 0;
+        
+        for (int i = s.size() - 1; i >= 0; i--) {
+            int curValue = romanMap[s[i]];
+            
+            if (curValue < preValue) {
+                result -= curValue;
+            } else {
+                result += curValue;
+            }
+            
+            preValue = curValue;
+        }
+        
+        return result;
+    }
+};
