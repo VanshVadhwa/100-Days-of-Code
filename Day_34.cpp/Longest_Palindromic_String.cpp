f943625fc09c272652c4c8c8c132a5a5154654d1
Vansh Vadhwa <vvad0647@gmail.com>
2024-02-15 22:15:02 +0530

    Add files via upload

---
+++ Day_34.cpp/Longest_Palindromic_String.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n == 0) return "";
+
+        int start = 0;
+        int maxLength = 1;
+
+        for (int i = 0; i < n; ++i) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = max(len1, len2);
+            if (len > maxLength) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
+
+private:
+    int expandAroundCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+};
