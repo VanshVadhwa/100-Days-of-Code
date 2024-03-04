be25a76b074bfe8e974cbde6467b95d5d8559b3f
Vansh Vadhwa <vvad0647@gmail.com>
2024-03-04 18:46:32 +0530

    Add files via upload

---
+++ Day_52.cpp/Set_Matrix_Zeroes.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        bool firstRowZero = false;
+        bool firstColZero = false;
+        
+        for (int j = 0; j < cols; ++j) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        
+        for (int i = 0; i < rows; ++i) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+        
+        for (int i = 1; i < rows; ++i) {
+            for (int j = 1; j < cols; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < rows; ++i) {
+            for (int j = 1; j < cols; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if (firstRowZero) {
+            for (int j = 0; j < cols; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if (firstColZero) {
+            for (int i = 0; i < rows; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
