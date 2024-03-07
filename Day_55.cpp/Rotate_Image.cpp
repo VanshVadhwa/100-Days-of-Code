23d7b6ea0bffbcf2bc4afb35ac92840cc2eb85f8
Vansh Vadhwa <vvad0647@gmail.com>
2024-03-07 19:06:16 +0530

    Add files via upload

---
+++ Day_55.cpp/Rotate_Image.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                ++start;
+                --end;
+            }
+        }
+    }
+};
