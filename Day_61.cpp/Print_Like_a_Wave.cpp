694f1f5b61d91fbc862a24551017f56a0c57f6bc
Vansh Vadhwa <vvad0647@gmail.com>
2024-03-13 20:59:15 +0530

    Add files via upload

---
+++ Day_61.cpp/Print_Like_a_Wave.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    vector<int> ans;
+    for(int col=0;col<mCols;col++) {
+        if(col&1) {
+            // odd Index --> Bottom to Top
+            for(int row=nRows-1;row>=0;row--) {
+                ans.push_back(arr[row][col]);
+            }
+        }
+
+        else {
+            //Even Index --> Top to Bottom
+            for(int row=0;row <nRows;row++) {
+                ans.push_back(arr[row][col]);
+            }
+        }
+    }
+    return ans;
+}
